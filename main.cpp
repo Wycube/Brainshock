d3c936d8e24d46edcbc0e808e78e3695f9c1a7de
Spencer Burton <wycube7@gmail.com>
2020-07-07 22:11:54 +0000

    Added command line argument parsing and 3 flags

--- main.cpp
+++ main.cpp
@@ -6,46 +6,96 @@
 #include <fstream>
 #include <chrono>
 #include <string>
+#include <unordered_map>
+
+#include "src/include/Brainf.hpp"
 
 //Semantic Versioning
-int major = 0;
-int minor = 1;
-int patch = 3;
+const int major = 0;
+const int minor = 2;
+const int patch = 0;
 
-int main(int argc, char *argv[]) {
-	std::cout << "Copyright (c) 2019 Spencer Burton" << std::endl;
-	std::cout << "Brainshock Alpha v" << major << '.' << minor << '.' << patch << std::endl;
-	
-	bs::frick::StdHWInterpreter interp(1);
+static std::unordered_map<std::string, int> strToNum = {
+	{"-help", 0}, {"h", 0},    //Print out the usage message
+	{"-version", 1}, {"v", 1}, //Print out version info
+	{"b", 2}                   //Print out runtime after execution
+};
 
-	interp.m_flags.numInput = true;
+static struct {
+	bool flags[3] = {false};
+	std::string path = "";
+	bool repl = true;
+} options;
 
-	bool useFile = false;
-
-	char result;
-
-	//Prompt whether to use a file for program
-	while(true) {
-		if(argc > 1) {
-			useFile = true;
-			break;
-		}
-
-		std::cout << "File y/n:" << std::endl;
-
-		std::cin >> result;
-
-		if(result == 'y') { 
-			useFile = true;
-			break;
-		} else if(result == 'n') {
-			useFile = false;
-			break;
-		}
-
+/*
+ * Checks if a commandline argument is a valid option, 
+ * and sets it if such.
+ */
+bool isOption(const std::string &option) {
+	if(strToNum.count(option)) {
+		options.flags[strToNum[option]] = true;
+		return true;
 	}
 
-	std::string program;
+	return false;
+}
+
+/*
+ * Loops through cl arguments to find valid options,
+ * a source file, and invalid args.
+ */
+void parseArgs(int argc, char *argv[]) {
+	//It starts at 1 because 0 is just the program name
+	if(argc < 2) {
+		return;
+	}
+
+	//Go through args for flags and the source file path
+	for(size_t i = 1; i < argc; i++) {
+		if(argv[i][0] == '-') {
+			if(!isOption(std::string(argv[i]).substr(1))) {
+				std::cout << "Error: " << argv[i] << " is not a valid option." << std::endl;
+				exit(1);
+			}
+		} else {
+			if(!options.path.empty()) {
+				std::cout << "Error: Multiple source files provided only one expected." << std::endl;
+				exit(2);
+			} else {
+				options.path = argv[i];
+				options.repl = false;
+			}
+		}
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	//Set flags and stuff
+	parseArgs(argc, argv);
+
+	//Should it print help or version info
+	if(options.flags[0]) {
+		std::cout << "Brainshock, Brainf*** Interpreter\n\n"
+		<< "Usage: " << argv[0] << " [options] [source_file]\n\n"
+		<< "Options:\n"
+		<< " -h --help    Prints this help message"
+		<< " -v --version Prints verision info"
+		<< " -b           Times the programs runtime and prints it after execution"
+		<< std::endl;
+
+		exit(0);
+	} else if(options.flags[1]) {
+		std::cout << "Brainshock, Brainf** Interpreter\n"
+		<< "Copyright (c) 2019 Spencer Burton"
+		<< "Version: " << major << '.' << minor << '.' << patch
+		<< std::endl;
+
+		exit(0);
+	}
+
+
+
 
 	//Get path of file and load it int 
 	if(useFile) {
@@ -81,32 +131,5 @@ int main(int argc, char *argv[]) {
 			std::cout << "Unable to open file" << std::endl;
 			return 1;
 		}
-	} else {
-		std::cout << "Input Program:";
-
-		while(program == "")
-			std::getline(std::cin, program);
 	}
-
-	if(interp.loadProgram(program.c_str())) {
-		std::cout << "successfully loaded program" << std::endl;
-	} else {
-		std::cout << "Error in program" << std::endl;
-		return 1;
-	}
-
-	//Benchmarking
-	auto start = std::chrono::steady_clock::now();
-
-	//Run
-	interp.run();
-
-	//Benchmarking
-	auto end = std::chrono::steady_clock::now();
-
-	auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-
-	interp.m_tape.printTape(interp.m_cellPtr); 
-
-	std::cout << "finished in " << duration.count() << " seconds" << std::endl;
 }
