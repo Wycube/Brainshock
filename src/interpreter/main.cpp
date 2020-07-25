8c97b1f0beefbcea3c1264878894a723b55e0a21
Spencer Burton <wycube7@gmail.com>
2020-07-25 10:34:00 -0700

    Added commands to the REPL, seperated parts of it to other functions

--- src/interpreter/main.cpp
+++ src/interpreter/main.cpp
@@ -16,6 +16,8 @@ const int major = 0;
 const int minor = 2;
 const int patch = 0;
 
+//---------- Command Line / Args ----------//
+
 static std::unordered_map<std::string, int> strToNum = {
 	{"-help", 0}, {"h", 0},    //Print out the usage message
 	{"-version", 1}, {"v", 1}, //Print out version info
@@ -73,32 +75,80 @@ void parseArgs(int argc, char *argv[]) {
 	}
 }
 
+
+//---------- REPL Stuff ----------//
+
+//This will tell the program which commands are set or input
+struct CommandFlags {
+	bool exit = false; //Exits the REPl
+	bool prog = false; //Displays the program that is being interpreted after processing and such
+	bool dump = false; //Dumps the memory in a certain base
+	bs::DUMP_BASE dumpBase;
+	bool time = false; //Prints the amount of time the interpreter took to run the last program
+	bool mem = false;  //Prints the current cells value and some around it, in decimal
+
+	void clear() {
+		exit = false;
+		prog = false;
+		dump = false;
+		time = false;
+		mem  = false;
+	}
+} comflags;
+
+//A helper function for finding out if and which command was inputted
+bool parseCommand(std::string input) {
+	if(input == "exit")           comflags.exit = true;
+	else if(input == "prog")      comflags.prog = true;
+	else if(input == "hexdump") { comflags.dump = true; comflags.dumpBase = bs::BASE_HEX; }
+	else if(input == "decdump") { comflags.dump = true; comflags.dumpBase = bs::BASE_DEC; }
+	else if(input == "bindump") { comflags.dump = true; comflags.dumpBase = bs::BASE_BIN; }
+	else if(input == "time")      comflags.time = true;
+	else if(input == "mem")       comflags.mem = true;
+	else return false;
+
+	return true;
+}
+
+//Prints whatever the set commands are suppose to
+void printInfo(bs::BrainfInterpreter &interpreter, std::chrono::microseconds runtime) {
+	bs::Program &program = interpreter.getProgram();
+
+	if(comflags.prog) {
+		std::cout << "Program: ";
+		for(std::size_t i = 0; i < program.length(); i++)
+			std::cout << program[i] << (program.processed ? program.tokens[i].data : static_cast<char>(0));
+		std::cout << std::endl;
+	} 
+	if(comflags.dump)
+		interpreter.getMemory().fDump(comflags.dumpBase, true); //I'll just keep the ascii on
+	if(comflags.mem)
+		interpreter.getMemory().fPrint(interpreter.getDataPtr());
+	if(comflags.time)
+		std::cout << "Finished in " << static_cast<double>(runtime.count() / 1000.0) << "ms" << std::endl; //Divide by a thousand for milliseconds
+}
+
 /*
- * A basic REPL with the exit being the word exit only, as input.
- * This does not pre-process the input string, but just runs it 
- * directly in the interpreter.
+ * A basic REPL with the commands aswell.
  */
 void evalLoop(bs::BrainfInterpreter &interpreter, std::stringbuf &buffer) {
 	std::string input;
-	std::chrono::milliseconds delta;
+	std::chrono::microseconds delta;
 
 	unsigned int optLevel = options.flags[5] ? 2 : options.flags[4] ? 1 : 0; //Optimization level, 2, 1, or 0(none)
 
 	while(true) {
 		std::cout << ": "; //This symbol is arbitrary I just needed something thats not a brainf*** instruction
-
 		std::getline(std::cin, input);
 
-		if(input == "exit")
-			return;
-		else if(input == "prog") { //Should replace with a better version soon
-			for(size_t i = 0; i < interpreter.getProgram().length(); i++)
-				std::cout << interpreter.getProgram()[i] << (interpreter.getProgram().processed ? interpreter.getProgram().tokens[i].data : 1);
-			std::cout << std::endl;
-		}
+		//Command stuff, if it's not a command run it in the interpreter
+		if(parseCommand(input)) {
+			if(comflags.exit)
+				return;
+
 		//-p Process the program if set 
 		//-O1 and -O2 Specific optimizations
-		if(!interpreter.loadProgram(input.c_str(), options.flags[3], false, optLevel)) {
+		} else if(!interpreter.loadProgram(input.c_str(), options.flags[3], false, optLevel)) {
 			std::cerr << "Error: " << interpreter.getError() << std::endl;
 		} else {
 			//Timing Start
@@ -111,23 +161,23 @@ void evalLoop(bs::BrainfInterpreter &interpreter, std::stringbuf &buffer) {
 
 			//Timing End
 			auto end = std::chrono::steady_clock::now();
-			delta = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+			delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 		}
 
-		interpreter.getMemory().fDump(bs::BASE_HEX, true);
-		//interpreter.getMemory().fPrint(interpreter.getDataPtr());
-
-		//-b Print Timing
-		if(options.flags[2])
-			std::cout << "Finished in " << delta.count() << "ms" << std::endl;
-		else if(buffer.str() != "")\
+		//Determine if the program output anything and clear the buffer
+		if(buffer.str() != "") {
 			std::cout << buffer.str() << std::endl;
+			buffer.str("");
+		}
 
-		//clear the buffer
-		buffer.str("");
+		//Print info from commands then clear them
+		printInfo(interpreter, delta);
+		comflags.clear();
 	}
 }
 
+
+//---------- Main ---------//
 
 int main(int argc, char *argv[]) {
 	//Set flags and stuff
@@ -136,7 +186,7 @@ int main(int argc, char *argv[]) {
 	//Should it print help or version info
 	if(options.flags[0]) {
 		std::cout << "Brainshock, Brainf*** Interpreter\n\n"
-		<< "Usage: " << argv[0] << " [options] [source_file]\n\n"
+		<< "Usage: " << argv[0] << " [options...] [source_file]\n\n"
 		<< "Options:\n"
 		<< " -h --help    Display this help message\n"
 		<< " -v --version Display version info\n"
@@ -144,7 +194,7 @@ int main(int argc, char *argv[]) {
 		<< " -b           Display the program's runtime after execution\n"
 		<< " -p           Processes/Optimizes the program to an IR before it's ran\n"
 		<< " -O1          Optimizes the processed program significantly\n"
-		<< " -O2          Optimizes the processed program a bit more"
+		<< " -O2          Optimizes the processed program a bit more past O1"
 		<< std::endl;
 
 		return 0;
@@ -159,7 +209,6 @@ int main(int argc, char *argv[]) {
 
 	//Go into interactive mode
 	if(options.repl) {
-		//Create Interpreter for next steps and ostream plus buffer
 		std::stringbuf buffer;
 		std::ostream stream(nullptr);
 		stream.rdbuf(&buffer);
